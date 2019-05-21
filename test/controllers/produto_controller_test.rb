2faa4a92d80156dfc764c4893443ea59bd01b922
rbuzatto <mightyrafa@gmail.com>
2019-05-21 00:25:57 -0300

    include test for seach product

--- test/controllers/produto_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ProdutoControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get produto_index_url
-    assert_response :success
-  end
-
-end
--- test/integration/search_products_test.rb
+++ test/integration/search_products_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class SearchProductsTest < ActionDispatch::IntegrationTest
-  test "the truth" do
-    assert true
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+
+  test "should return one product w/ 10% discount for 'hot' search input" do
+    visit "/"
+    fill_in "nome", with: "hot"
+    click_button "", class: "btn btn-secondary my-2 my-sm-0"
+    assert_css('.product-card', count: 1)
+    assert_css('.product-card__promotion', count: 1)
+    assert_text('10% OFF')
+    assert_text ('1 produto encontrado!')
+  end
+
+  test "should return two products for 'pepper' search input" do
+    visit "/"
+    fill_in "nome", with: "pepper"
+    click_button "", class: "btn btn-secondary my-2 my-sm-0"
+    assert_css('.product-card', count: 2)
+    assert_css('.product-card__promotion', count: 1)
+    assert_text ('2 produtos encontrados!')
   end
 end
